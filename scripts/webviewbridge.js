edafa104c7f4734682c5de29eece101a4a75671a
Byunghan Yoo <yoobh17@gmail.com>
2019-12-13 02:56:20 +0900

    refactor

--- scripts/webviewbridge.js
+++ scripts/webviewbridge.js
@@ -1,25 +1,25 @@
-(function (window) {
-  'use strict';
+(function(window) {
+  "use strict";
 
   //Make sure that if WebViewBridge already in scope we don't override it.
   if (window.WebViewBridge) {
     return;
   }
 
-  var RNWBSchema = 'wvb';
+  var RNWBSchema = "wvb";
   var sendQueue = [];
   var receiveQueue = [];
   var doc = window.document;
-  var customEvent = doc.createEvent('Event');
+  var customEvent = doc.createEvent("Event");
 
   function callFunc(func, message) {
-    if ('function' === typeof func) {
+    if ("function" === typeof func) {
       func(message);
     }
   }
 
   function signalNative() {
-    window.location = RNWBSchema + '://message' + new Date().getTime();
+    window.location = RNWBSchema + "://message" + new Date().getTime();
   }
 
   //I made the private function ugly signiture so user doesn't called them accidently.
@@ -27,16 +27,16 @@
   var WebViewBridge = {
     //this function will be called by native side to push a new message
     //to webview.
-    __push__: function (message) {
+    __push__: function(message) {
       receiveQueue.push(message);
       //reason I need this setTmeout is to return this function as fast as
       //possible to release the native side thread.
-      setTimeout(function () {
+      setTimeout(function() {
         var message = receiveQueue.pop();
         callFunc(WebViewBridge.onMessage, message);
       }, 15); //this magic number is just a random small value. I don't like 0.
     },
-    __fetch__: function () {
+    __fetch__: function() {
       //since our sendQueue array only contains string, and our connection to native
       //can only accept string, we need to convert array of strings into single string.
       var messages = JSON.stringify(sendQueue);
@@ -49,9 +49,12 @@
     },
     //make sure message is string. because only string can be sent to native,
     //if you don't pass it as string, onError function will be called.
-    send: function (message) {
-      if ('string' !== typeof message) {
-        callFunc(WebViewBridge.onError, "message is type '" + typeof message + "', and it needs to be string");
+    send: function(message) {
+      if ("string" !== typeof message) {
+        callFunc(
+          WebViewBridge.onError,
+          "message is type '" + typeof message + "', and it needs to be string"
+        );
         return;
       }
 
@@ -67,6 +70,6 @@
   window.WebViewBridge = WebViewBridge;
 
   //dispatch event
-  customEvent.initEvent('WebViewBridge', true, true);
+  customEvent.initEvent("WebViewBridge", true, true);
   doc.dispatchEvent(customEvent);
-}(window));
+})(window);
